6bbafe531c2378f460dc903d994b529bc04b9871
Ryan Nichols <ryan3nichols@gmail.com>
2022-12-20 15:16:51 -0500

    Responsive header: cypress CI hotfix

--- client/cypress/e2e/home.cy.js
+++ client/cypress/e2e/home.cy.js
@@ -1,8 +1,5 @@
 it('should load the page', () => {
   cy.visit('/');
   cy.get('div[data-testid=home-page]').should('exist');
-  cy.findAllByText(/This is a full stack project launcher (WIP)/i).should(
-    'have.length',
-    1
-  );
+  cy.contains('This is a full stack project launcher (WIP)').should('exist');
 });
