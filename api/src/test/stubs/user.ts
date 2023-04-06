02acc763346aa7a91f80d234f94c53ed08cf9a34
Ryan Nichols <ryan3nichols@gmail.com>
2023-04-06 17:41:48 -0400

    Hot fixes for CI/CD

---
+++ api/src/test/stubs/user.ts
@@ -0,0 +1,23 @@
+const user1 = {
+  email: 'ryan3nichols@gmail.com',
+  username: 'god',
+};
+
+const user2 = {
+  email: 'john.smith@email.com',
+  username: 'john.smith',
+};
+
+const user3 = {
+  email: 'jane.doe@email.com',
+  username: 'jane.does',
+};
+
+const user4 = {
+  email: 'bill.bruh@email.com',
+  username: 'bill.bruh',
+};
+
+const allUsers = [user1, user2, user3, user4];
+
+export { allUsers };
