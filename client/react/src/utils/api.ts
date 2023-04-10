9e07cab6bbc22a308ac7993ecf85aacec576b905
Ryan Nichols <ryan3nichols@gmail.com>
2023-04-10 11:23:29 -0400

    Process env (#21)
    * Remove prisma
    
    * Now can have multiple frameworks under clients folder
    
    * Changed backend file structure, fixes for api and docker compose to get sequelize working
    
    * Backend tests running no errors in docker compose file with seed data passed to postgres service
    
    * trouble shooting api url env var for react cloud run deploy

--- client/react/src/utils/api.ts
+++ client/react/src/utils/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const apiBaseUrl = process.env.REACT_APP_API_URL;
+const apiBaseUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_API_URL
+    : process.env.REACT_APP_API_URL;
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
 });
